ba9a3f800bef5484e36d9441605252641f9047f5
Nicholas Watts <thekilowat@gmail.com>
2025-11-08 17:50:39 -0600

    Initial Commit

---
+++ NMC-HackUTD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NMC-HackUTD
+//
+//  Created by Nick Watts on 11/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NMC-HackUTD/NMC_HackUTDApp.swift
@@ -0,0 +1,17 @@
+//
+//  NMC_HackUTDApp.swift
+//  NMC-HackUTD
+//
+//  Created by Nick Watts on 11/8/25.
+//
+
+import SwiftUI
+
+@main
+struct NMC_HackUTDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
